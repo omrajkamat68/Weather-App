c5f8f03254f3a0b6ac3b67c251c88b482e5a18d6
omrajkamat68 <ob2614@srmist.edu.in>
2024-07-06 13:24:54 +0530

    first commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:weatherapp_starter_project/controller/global_controller.dart';
 import 'package:weatherapp_starter_project/utils/custom_colors.dart';
-import 'package:weatherapp_starter_project/widgets/comfort_level.dart';
 import 'package:weatherapp_starter_project/widgets/current_weather_widget.dart';
 import 'package:weatherapp_starter_project/widgets/daily_data_forecast.dart';
 import 'package:weatherapp_starter_project/widgets/header_widget.dart';
@@ -66,9 +65,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     const SizedBox(
                       height: 10,
                     ),
-                    ComfortLevel(
-                        weatherDataCurrent:
-                            globalController.getData().getCurrentWeather())
+
                   ],
                 ),
               )),
